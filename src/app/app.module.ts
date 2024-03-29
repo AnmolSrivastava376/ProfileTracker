46a596d47331164cf6f419966354c4eae7acc6bb
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-03-29 21:23:48 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfilePageComponent } from './Components/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
