eeb1266d2b745292c810ba0ddb385ae6cba268c6
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-03-30 23:55:14 +0530

    component/profile-page

--- src/app/Components/profile-page/profile-page.component.ts
+++ src/app/Components/profile-page/profile-page.component.ts
@@ -18,7 +18,6 @@ export class ProfilePageComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       this.profiles = this.dataService.getProfiles();
       if (this.profiles.length === 0) {
-        // Fetch profiles from API if profiles array is empty
         this.dataService.fetchProfiles().subscribe((profiles: Profile[]) => {
           this.profiles = profiles;
           this.fetchSelectedProfile();
@@ -35,7 +34,6 @@ export class ProfilePageComponent implements OnInit {
         const id: number = parseInt(idParam, 10);
         this.selectedProfile = this.profiles.find(profile => profile.id === id);
         this.loading = false;
-        console.log(this.selectedProfile);
       }
     });
   }
